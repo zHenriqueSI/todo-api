bddcd4f00db09cd13181ed8d741e6c4f15c626b2
henriquegomesnunes <henriquegomesnunes19@usp.br>
2023-02-25 10:15:30 -0300

    feat: tasks list

--- files/main.py
+++ files/main.py
@@ -17,3 +17,20 @@ class TodoTask(BaseModel):
 if not os.path.exists('tasks.pkl'):
     with open('tasks.pkl', 'wb') as arq:
         pickle.dump([], arq)
+
+
+@app.get('/tasks_list/{option}')
+def tasks_list(option: str):
+    with open('tasks.pkl', 'rb') as arq:
+        tasks_list = pickle.load(arq)
+
+    if option == "all":
+        return tasks_list
+    elif option == "done":
+        tasks_list = list(filter(lambda x: x.done == True, tasks_list))
+        return tasks_list
+    elif option == "notdone":
+        tasks_list = list(filter(lambda x: x.done == False, tasks_list))
+        return tasks_list
+    else:
+        return {"error": "error when trying to read the tasks list"}
